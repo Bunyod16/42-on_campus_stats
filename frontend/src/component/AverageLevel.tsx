ada7255deda84f37f9bba14f39912e0bc12536e5
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-02-04 22:25:39 +0800

    feat: add api

--- frontend/src/component/AverageLevel.tsx
+++ frontend/src/component/AverageLevel.tsx
@@ -1,5 +1,6 @@
 import { Card, H1Style } from "../styles";
 import tw from "twin.macro";
+import React from "react";
 
 interface IAverageLevelProps {
   className?: string;
@@ -10,7 +11,30 @@ const NumberDisplay = tw.div`
 `;
 
 const AverageLevel = ({ className }: IAverageLevelProps) => {
-  const averageLvl = 4.5;
+  const [averageLvl, setAverageLvl] = React.useState(0);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      await fetch("/api/on-campus/average-user-level")
+        .then((response) => {
+          if (response.ok) {
+            console.log("Successfully fetch Average User Level");
+            return response.json();
+          }
+        })
+        .then((data) => setAverageLvl(data.average_level))
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+    fetchData();
+
+    // Call the API every 5 minutes
+    const interval = setInterval(fetchData, 1000 * 60 * 5);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
   return (
     <Card className={className + " flex flex-col"}>
       <H1Style>Average level</H1Style>
