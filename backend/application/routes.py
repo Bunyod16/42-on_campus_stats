6ab6e18dc12f2bb20b61cccbcb6bcbf2804c6222
Bunyod16 <bunyodshams@gmail.com>
2023-02-05 19:57:55 +0800

    chore: changed env var

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-token = Token(int(os.getenv("42_CAMPUS_ID")), os.getenv("CAMPUS_UID"), os.getenv("CAMPUS_SECRET"))
+token = Token(int(os.getenv("CAMPUS_ID")), os.getenv("CAMPUS_UID"), os.getenv("CAMPUS_SECRET"))
 
 @app.errorhandler(404)
 def page_not_found(e):
