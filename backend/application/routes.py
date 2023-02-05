7cbfca636096855f50a01a2bc5571da9cc9d230d
Bunyod16 <bunyodshams@gmail.com>
2023-02-05 21:07:04 +0800

    changed cache time

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -18,12 +18,12 @@ def index():
     return (f"API IS LIVE")
 
 @app.route('/api/on-campus/active-users', methods=['GET'])
-@cache.cached(timeout=120)
+@cache.cached(timeout=30)
 def active_users():
     return (jsonify(token.get_active_users()))
 
 @app.route('/api/on-campus/active-user-projects', methods=['GET'])
-@cache.cached(timeout=120)
+@cache.cached(timeout=60)
 def user_projects():
     return (jsonify(token.active_user_projects()))
 
@@ -33,12 +33,12 @@ def feature():
     return (jsonify(token.average_user_level()))
 
 @app.route('/api/on-campus/average-session-hours', methods=['GET'])
-@cache.cached(timeout=120)
+@cache.cached(timeout=160)
 def average_session_hours():
     return (jsonify(token.average_session_hours()))
 
 @app.route('/api/most-recent-submission', methods=['GET'])
-@cache.cached(timeout=120)
+@cache.cached(timeout=180)
 def most_recent_submission():
     return (jsonify(token.most_recent_submission()))
 
@@ -48,7 +48,7 @@ def cadet_pisciner_ratio():
     return (jsonify(token.cadet_pisciner_ratio()))
 
 @app.route('/api/on-campus/active-user-skills', methods=['GET'])
-@cache.cached(timeout=120)
+@cache.cached(timeout=180)
 def active_user_skills():
     return (jsonify(token.active_users_skills()))
 
