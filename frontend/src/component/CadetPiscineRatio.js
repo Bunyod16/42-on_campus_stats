19449357ab36f07006eecf392e4f7abc55eddf9d
Jason <jasonkwm13@gmail.com>
2023-02-13 18:12:07 +0800

    fix cadet piscine ratio and spider chart labels 'web' overflow

--- frontend/src/component/CadetPiscineRatio.js
+++ frontend/src/component/CadetPiscineRatio.js
@@ -1,11 +1,14 @@
-import { useEffect, useState } from "react";
-import * as d3 from "d3";
+import { useEffect, useState, useRef } from "react";
 import { Card, H1Style } from "../styles";
 import "../styles/ratio.css";
+import { useDimensions } from "../hooks/useDimension";
 import axios from "axios";
 // Ratio Bar for Cadets vs Pisciners
 export default function CadetPiscineRatio(props) {
   const [barWidth, setBarWidth] = useState({cadet:0,pisciner:0});
+  const cardRef = useRef();
+  const dimension = useDimensions(cardRef);
+
   useEffect( () =>{
     let fetchRatio = async () =>{
       await axios.get("/on-campus/cadet-pisciner-ratio")
@@ -13,12 +16,16 @@ export default function CadetPiscineRatio(props) {
               let data = res.data
               let parseData = {cadets : parseInt(data["cadets"]), pisciners: parseInt(data["pisciners"])};
               let total = parseInt(parseData["cadets"]) + parseInt(parseData["pisciners"]);
-              let widthCadet = (parseData["cadets"] / total) * 395;
-              let widthPiscine = (parseData["pisciners"] / total) * 395;
+              let widthCadet = (parseData["cadets"] / total) * (dimension.width * .8);
+              let widthPiscine = (parseData["pisciners"] / total) * (dimension.width * .8);
               setBarWidth({cadet: widthCadet, pisciner: widthPiscine});
             })
             .catch ( err => {
-            console.log(err);
+              // set default width to 50/50 if api request fail
+              let widthCadet = (50 / 100) * (dimension.width * .8);
+              let widthPiscine = (50 / 100) * (dimension.width * .8);
+              setBarWidth({cadet: widthCadet, pisciner: widthPiscine});
+              console.log(err);
             })
     }
     fetchRatio();
@@ -26,16 +33,16 @@ export default function CadetPiscineRatio(props) {
     return () => clearInterval(interval);
   }, []);
   return (
-    <Card>
+    <Card ref={cardRef}>
         <H1Style><span className="h1-cadets">Cadets</span> to <span className="h1-piscine">Piscine</span> ratio</H1Style>
-        <svg width="400px" height="20px" className="cadet-piscine-ratio-svg">
+        <svg width={(dimension.width * .8) + 5} height="20px" className="cadet-piscine-ratio-svg">
             <g>
                 <rect className="cadet-rect" width={barWidth["cadet"]} height="20px" rx="5"></rect>
-                <text x={barWidth["cadet"] / 2} y="15" className="cadet-piscine-ratio-text">Cadet</text>
+                <text x={(barWidth["cadet"] / 2) - 21} y="15" className="cadet-piscine-ratio-text">Cadet</text>
             </g>
             <g>
-                <rect className="piscine-rect" width={barWidth["pisciner"]} height="20px" x={barWidth["cadet"] + 5} rx="5"></rect>
-                <text x={barWidth["cadet"] + (barWidth["pisciner"] / 2)} y="15" className="cadet-piscine-ratio-text">Piscine</text>
+                <rect className="piscine-rect"  width={barWidth["pisciner"]} x={barWidth["cadet"] + 5} height="20px" rx="5"  ></rect>
+                <text x={barWidth["cadet"] + 5 + (barWidth["pisciner"] / 2) - 26} y="15" className="cadet-piscine-ratio-text">Piscine</text>
             </g>
         </svg>
     </Card>
