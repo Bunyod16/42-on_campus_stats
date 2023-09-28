49bee6cbfbd4f72251578aceb0736f7011360d1e
Jason <jasonkwm13@gmail.com>
2023-09-28 14:45:04 +0800

    feat: add page reload every hour

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,74 +14,77 @@ import TopFarmers from "./component/TopFarmers";
 // todo dynamic width/height based on whichever is narrower on screen
 
 function App() {
-  const transitionTime = 20000;
-  const [currentComponent, setCurrentComponent] = useState("WeeklyCadetXp");
+    const transitionTime = 20000;
+    const [currentComponent, setCurrentComponent] = useState("WeeklyCadetXp");
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentComponent((currentComponent) =>
-        currentComponent === "WeeklyCadetXp"
-          ? "TotalActiveUser7Days"
-          : "WeeklyCadetXp"
-      );
-    }, transitionTime);
-    return () => clearInterval(intervalId);
-  }, []);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentComponent((currentComponent) =>
+                currentComponent === "WeeklyCadetXp" ? "TotalActiveUser7Days" : "WeeklyCadetXp"
+            );
+        }, transitionTime);
+        return () => clearInterval(intervalId);
+    }, []);
 
-  const [className1, setClassName1] = useState(
-    "transition-opacity duration-500 delay-300 opacity-100"
-  );
+    const [className1, setClassName1] = useState("transition-opacity duration-500 delay-300 opacity-100");
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setClassName1(
-        currentComponent === "WeeklyCadetXp"
-          ? "transition-opacity opacity-0 hidden duration-600 !p-0 !border-0 !mt-0"
-          : "transition-opacity duration-500 delay-200 opacity-100"
-      );
-    }, 1000);
-    return () => clearTimeout(timeoutId);
-  }, [currentComponent]);
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setClassName1(
+                currentComponent === "WeeklyCadetXp"
+                    ? "transition-opacity opacity-0 hidden duration-600 !p-0 !border-0 !mt-0"
+                    : "transition-opacity duration-500 delay-200 opacity-100"
+            );
+        }, 1000);
+        return () => clearTimeout(timeoutId);
+    }, [currentComponent]);
 
-  const [className2, setClassName2] = useState(
-    "transition-opacity opacity-0 duration-600 h-0 !p-0 !border-0 !mt-0"
-  );
+    const [className2, setClassName2] = useState("transition-opacity opacity-0 duration-600 h-0 !p-0 !border-0 !mt-0");
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setClassName2(
-        currentComponent === "WeeklyCadetXp"
-          ? "transition-opacity duration-500 delay-200 opacity-100"
-          : "transition-opacity duration-600 opacity-0 hidden !p-0 !border-0 !mt-0"
-      );
-    }, 1000);
-    return () => clearTimeout(timeoutId);
-  }, [currentComponent]);
-  return (
-    <div className="text-center h-screen flex flex-col items-center bg-gray-800  min-w-[1200px] min-h-[1200px] text-base">
-      <div className="bg-gray-900 text-3xl flex flex-col items-center justify-center text-white py-4 w-full">
-        <h1>Live Stats: 42 Kuala Lumpur</h1>
-      </div>
-      <div className="flex w-full gap-4 xl:max-w-[80%] content-center m-4 px-4 h-full">
-        <div className="flex-col inline-flex gap-4 max-h-full flex-[2]">
-          <ActiveUserProjects className="" />
-          <CurrentActiveUser className="flex-1" />
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setClassName2(
+                currentComponent === "WeeklyCadetXp"
+                    ? "transition-opacity duration-500 delay-200 opacity-100"
+                    : "transition-opacity duration-600 opacity-0 hidden !p-0 !border-0 !mt-0"
+            );
+        }, 1000);
+        return () => clearTimeout(timeoutId);
+    }, [currentComponent]);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            window.location.reload();
+            // reload page every hour
+        }, 1000 * 60 * 60 * 1);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <div className="flex h-screen min-h-[1200px] min-w-[1200px] flex-col items-center  bg-gray-800 text-center text-base">
+            <div className="flex w-full flex-col items-center justify-center bg-gray-900 py-4 text-3xl text-white">
+                <h1>Live Stats: 42 Kuala Lumpur</h1>
+            </div>
+            <div className="m-4 flex h-full w-full content-center gap-4 px-4 xl:max-w-[80%]">
+                <div className="inline-flex max-h-full flex-[2] flex-col gap-4">
+                    <ActiveUserProjects className="" />
+                    <CurrentActiveUser className="flex-1" />
+                </div>
+                <div className="inline-flex max-h-full flex-[2] flex-col gap-4">
+                    <MostActiveUsers />
+                    <TopFarmers />
+                    <TotalActiveUser7Days className={className1} />
+                    <WeeklyCadetXp className={className2} />
+                    <CadetPiscineRatio className="h-full" />
+                </div>
+                <div className="inline-flex max-h-full flex-1 flex-col gap-4">
+                    <AverageLevel className="" />
+                    <AverageSessionTime className="" />
+                    <MostRecentSubmission className="" />
+                </div>
+            </div>
         </div>
-        <div className="flex-col inline-flex gap-4 max-h-full flex-[2]">
-          <MostActiveUsers />
-          <TopFarmers />
-          <TotalActiveUser7Days className={className1} />
-          <WeeklyCadetXp className={className2} />
-          <CadetPiscineRatio className="h-full" />
-        </div>
-        <div className="flex-col inline-flex gap-4 max-h-full flex-1">
-          <AverageLevel className="" />
-          <AverageSessionTime className="" />
-          <MostRecentSubmission className="" />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
